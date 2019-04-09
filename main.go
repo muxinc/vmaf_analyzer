3756aef604fb1e751c79338d43683ce4458f8949
Nick Chadwick <chadnickbok@gmail.com>
2019-04-09 12:53:44 -0700

    Fetch and parse master manifest

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+
+	"github.com/grafov/m3u8"
 )
 
 var (
@@ -20,9 +23,40 @@ func main() {
 		return
 	}
 
+	mezzanineFile := flag.Args()[0]
+	manifestURL := flag.Args()[1]
+
 	// Parse the viewer data
 
 	// Load the master manfest
+	resp, err := http.Get(manifestURL)
+	if err != nil {
+		fmt.Printf("Failed to fetch master manfiest (%s): %v\n", manifestURL, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	manifest, manifestType, err := m3u8.DecodeFrom(resp.Body, false)
+	if err != nil {
+		fmt.Printf("Failed to decode master manifest: %v", err)
+		return
+	}
+
+	var masterPlaylist *m3u8.MasterPlaylist
+	switch manifestType {
+	case m3u8.MASTER:
+		masterPlaylist = manifest.(*m3u8.MasterPlaylist)
+	default:
+		fmt.Printf("Invalid manifest format, must be a master manifest")
+		return
+	}
+
+	fmt.Printf("Master Playlist: %+v\n", masterPlaylist)
+	fmt.Printf("Loading mezzanine: %s\n", mezzanineFile)
+
+	for _, variant := range masterPlaylist.Variants {
+		fmt.Printf("Here's a variant: %v\n", variant)
+	}
 
 	// Parse bitrate & resolution combinations
 
